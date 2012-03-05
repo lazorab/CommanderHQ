ec918c408962e5b1799b53e01c64ef4a73277477
Darren Hart <Darren@Darren-PC.(none)>
2012-03-05 11:26:30 +0200

    added recipes module

--- modules/recipes/controller.php
+++ modules/recipes/controller.php
@@ -1,12 +1,53 @@
 <?php
 class RecipesController extends Controller
 {
+	var $Model;
+	
 	function __construct()
 	{
 		parent::__construct();
 		session_start();
 		if(!isset($_SESSION['UID']))
 			header('location: index.php?module=login');	
+		$this->Model = new RecipesModel;
+	}
+	
+	function html()
+	{
+		$html='';
+		
+		if(isset($_REQUEST['id']))
+		{
+			$Recipe = $this->Model->getRecipe($_REQUEST['id']);
+			$FormattedText = $this->getFormattedText($Recipe->Recipe);
+			$html.='<h3>'.$Recipe->Title.'</h3>';
+			$html.='<p>'.$FormattedText.'</p>';
+		}
+		else
+		{
+			$Recipes = $this->Model->getRecipes($_REQUEST);
+			foreach($Recipes as $Recipe)
+			{
+				$html.='<h3><a href="index.php?module=recipes&id='.$Recipe->recid.'">'.$Recipe->Title.'</a></h3>';
+			}
+		}	
+		return $html;
+	}
+	
+	private function getFormattedText($unFormattedText)
+	{
+		$FormattedText = '';
+		$Search = array();
+		$Replace = array();
+		$FoodItems = $this->Model->getFoodItems();
+		foreach($FoodItems as $Item)
+		{
+			array_push($Search, '{'.$Item->FoodName.'}');
+			array_push($Replace, '<a href="?module=foodlog&id='.$Item->recid.'">'.$Item->FoodName.'</a>');
+		}
+		$Text = str_replace('{br}','<br/>',$unFormattedText);
+		$FormattedText = str_replace($Search,$Replace,$Text);
+		return $FormattedText;
 	}
 	
 	function CustomHeader()
--- modules/recipes/model.php
+++ modules/recipes/model.php
@@ -1,7 +1,72 @@
 <?php
 class RecipesModel extends Model
 {
-
+	function __construct()
+	{
+		mysql_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD);
+		@mysql_select_db(DB_CUSTOM_DATABASE) or die("Unable to select database");	
+	}
 	
+	function getRecipes($_SEARCH)
+	{
+		$Recipes = array();
+		$SQL = 'SELECT recid, Title, Recipe FROM Recipes';
+		if(isset($_SEARCH['searchword']))
+			$SQL .= 'WHERE Title LIKE "'.$_SEARCH['searchword'].'%"';
+		$Result = mysql_query($SQL);	
+		while($Row = mysql_fetch_assoc($Result))
+		{
+			array_push($Recipes, new RecipeObject($Row));
+		}
+		return $Recipes;
+	}	
+	
+	function GetRecipe($Id)
+	{
+		$SQL = 'SELECT recid, Title, Recipe FROM Recipes WHERE recid = '.$Id.'';
+		$Result = mysql_query($SQL);	
+		$Row = mysql_fetch_assoc($Result);
+		$Recipe = new RecipeObject($Row);
+		
+		return $Recipe;
+	}	
+
+	function getFoodItems()
+	{
+		$FoodItems = array();
+		$Sql = 'SELECT recid, FoodName FROM FoodItems';
+		$Result = mysql_query($Sql);
+		while($Row = mysql_fetch_assoc($Result))
+		{
+			array_push($FoodItems, new FoodItems($Row));
+		}	
+		return $FoodItems;
+	}
+}
+
+class RecipeObject
+{
+	var $recid;
+	var $Title;
+	var $Recipe;
+
+	function __construct($Row)
+	{
+		$this->recid = $Row['recid'];
+		$this->Title = $Row['Title'];
+		$this->Recipe = $Row['Recipe'];
+	}
+}
+
+class FoodItems
+{
+	var $recid;
+	var $FoodName;
+
+	function __construct($Row)
+	{
+		$this->recid = $Row['recid'];
+		$this->FoodName = $Row['FoodName'];
+	}
 }
 ?>
