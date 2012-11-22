792a9f9c9b3b4cf1a442430483d3b20971a3e69d
Chris Stryczynski <chris@soundshare.co.uk>
2012-11-22 13:02:46 +0000

    changed config credentials

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -2,6 +2,6 @@
 
     define("DB_USERNAME", "bemobile");
     define("DB_PASSWORD", "peTerpanHouse!2012");
-    define("DB_SERVER", "localhost");
-    define("DB_CUSTOM_DATABASE", "bemobile_Commander");
+    define("DB_SERVER", "196.3.168.42");
+	define("DB_DATABASE", "bemobile_Core");
 ?>
